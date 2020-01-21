070e4fdb1307e8a6239402d0dcaa8e28476bf81b
Fernando Mellone <fernandomellone2@gmail.com>
2020-01-21 10:13:41 -0300

    se aplicaron ciertos cambios

--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,45 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var opciones = ['INICIO', 'CLIENTES', 'PROVEEDORES'];
+
+const c = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'asd123',
-  database : 'boutique' 
+  database : 'boutique'
 });
-
-var opciones = ['INICIO', 'CLIENTES', 'PROVEEDORES']
-
 
 /* GET home page. */
-router.get('/', async function(req, res) {
-  res.render('index', {
-    title: 'Express',
-    options: opciones,
-    session: true
-  });
-  connection.query('SELECT * FROM categorias', await function(err, rows, fields) {
-    if (err) throw err;
-      var lista = [];
+router.get('/', function(req, res, next) {
+  c.query('SELECT * FROM prendas', async function(err, rows, fields) {
+    if (err) {throw err};
+    var prendas = [];
+    await function selPrendas (){
       for (let i=0; i < rows.length; i++) {
-         lista.push(rows[i].categoria);
+        prendas.push(rows[i]);
       }
+    };
+  });
+  res.status(200).send({prendas});
+  next.render('index', {
+  title: 'Express',
+  options: opciones,
+  session: true
   });
 });
 
+router.get('/categorias', function(req, res) {
+  c.query('SELECT * FROM categorias', function(err, rows, fields) {
+    if (err) throw err;
+    var categorias = [];
+    for (let i=0; i < rows.length; i++) {
+        categorias.push(rows[i]);
+    }
+    res.status(200).send({
+      categorias
+    })
+  });
+});
 
 module.exports = router;
