56ef89faaff01eb3f3d2c5574fad0b9490ca46e4
Fernando Mellone <fernandomellone2@gmail.com>
2020-01-26 20:03:01 -0300

    se agregaron todas las vistas (faltan ajustes de funcionalidad)

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,8 @@ var conn = mysql.createConnection({
   pool: dbConfig.pool
 });
 
+
+// Ruta para la vista 'inicio'
 router.get('/', (req, res)=> {
   conn.query('SELECT * FROM index_view',
     (err, rows, fields) => {
@@ -29,36 +31,121 @@ router.get('/', (req, res)=> {
 });
 
 
-// Ruta para la vista prendas
-router.get('/prendas', function(req, res, next) {
-  conn.query('SELECT * FROM prendas', function(err, rows, fields) {
+//Ruta para la vista 'proveedores'
+router.get('/proveedores', function(req, res){
+  conn.query('SELECT * FROM proveedores', function(err, rows, fields) {
     if (err) throw err;
-    var prendas = [];
-    // console.log(prendas);
-    for (let i=0; i < rows.length; i++) {
-      prendas.push(rows[i]);
-      // console.log(fields);
+    var proveedores = [];
+    for (let i=0; i< rows.length; i++) {
+      proveedores.push(rows[i]);
     }
-    console.log(prendas);
     res.status(200).send({
-      prendas
-    });
+      proveedores
+    })
+  })
+});
+
+
+// Ruta para la vista 'clientes'
+router.get('/clientes', function(req, res) {
+  conn.query('SELECT * FROM clientes', function(err, rows, fields) {
+    if (err) throw err;
+    var clientes = [];
+    for (let i=0; i < rows.length; i++) {
+      clientes.push(rows[i]);
+    }
+    res.status(200).send({
+      clientes
+    })
   });
 });
 
 
-// Ruta para la vista categorías
+// Ruta para pop-up categorías
 router.get('/categorias', function(req, res) {
   conn.query('SELECT * FROM categorias', function(err, rows, fields) {
     if (err) throw err;
     var categorias = [];
     for (let i=0; i < rows.length; i++) {
-        categorias.push(rows[i]);
+      categorias.push(rows[i]);
     }
     res.status(200).send({
       categorias
     })
   });
 });
+
+// Ruta para la vista 'Ventas'
+router.get('/ventas', function(req, res) {
+  conn.query('SELECT * FROM sales_view', function(err, rows, fields) {
+    if (err) throw err;
+    var ventas = [];
+    for (let i=0; i < rows.length; i++) {
+      ventas.push(rows[i]);
+    }
+    res.status(200).send({
+      ventas
+    })
+  });
+});
+
+// Ruta para la vista 'Compras'
+router.get('/compras', function(req, res) {
+  conn.query('SELECT * FROM compras_view', function(err, rows, fields) {
+    if (err) throw err;
+    var compras = [];
+    for (let i=0; i < rows.length; i++) {
+      compras.push(rows[i]);
+    }
+    res.status(200).send({
+      compras
+    })
+  });
+});
+
+
+// Ruta para la vista 'Factura de venta'
+router.get('/fVenta/:nFact', function(req, res) {
+  conn.query(`SELECT * FROM boutique.sales_view WHERE n_factura=${req.params.nFact}`, function(err, rows, fields) {
+    if (err) throw err;
+    var fact_venta = [];
+    for (let i=0; i < rows.length; i++) {
+      fact_venta.push(rows[i]);
+    }
+    res.status(200).send({
+      fact_venta
+    })
+  });
+});
+
+// Ruta para la vista 'Factura de compra'
+router.get('/fCompra/:nFact', function(req, res) {
+  conn.query(`SELECT * FROM boutique.fact_compra_view WHERE n_factura=${req.params.nFact}`, function(err, rows, fields) {
+    if (err) throw err;
+    var fact_compra = [];
+    for (let i=0; i < rows.length; i++) {
+      fact_compra.push(rows[i]);
+    }
+    res.status(200).send({
+      fact_compra
+    })
+  });
+});
+
+
+// Ruta para la vista del modal prenda single
+router.get('/prenda/:id', function(req, res) {
+  conn.query(`SELECT * FROM boutique.single_prenda_view WHERE id_prenda=${req.params.id}`, function(err, rows, fields){
+    if (err) throw err;
+    var prenda = [];
+    for (let i=0; i < rows.length; i++) {
+      prenda.push(rows[i]);
+    }
+    res.status(200).send(
+      prenda
+    )
+  })
+})
+
 
 module.exports = router;
